e202221318aa683d2c5c49f9ca82f41e66035484
Tanos Gábor <tanosgabi@gmail.com>
2015-02-25 18:01:03 +0100

    elvileg kész

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,9 +3,11 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
+        if(number % 15==0)
+            return "fizzbuzz";
         if(number % 3==0)
             return "fizz";
-        if(number % 3==0)
+        if(number % 5==0)
             return "buzz";
         return String.valueOf(number);
     }
