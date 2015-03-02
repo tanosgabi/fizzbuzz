e6fa4746625b65b3c0571ccc945c3a49871bac4f
tanosgabi <tanosgabi@gmail.com>
2015-03-02 22:41:13 +0100

    a. ferladat

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,13 +3,13 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
-        if(number % 15==0)
-            return "fizzbuzz";
-        if(number % 3==0)
-            return "fizz";
-        if(number % 5==0)
-            return "buzz";
-        return String.valueOf(number);
+        String executeString="";
+        String num=String.valueOf(number);
+        if(number % 7==0)
+            executeString +="Wizz";
+        else
+            executeString =num;
+        return executeString;
     }
     
 }
